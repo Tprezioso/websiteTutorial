52419c70aa1b86340975ff8b9fc6d293627de6d4
Tprezioso <tommyprezioso@gmail.com>
2018-09-24 18:02:48 -0400

    cleaning up some code

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,35 +4,36 @@ import "./NavBar.css";
 
 class NavBar extends Component {
   render() {
-      return <nav className="navbar navbar-expand-md navbar navbar-light">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            React Website
-          </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"/>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to="/">
-                  Home <span className="sr-only">(current)</span>
+      return (<nav className="navbar navbar-expand-md navbar navbar-light">
+          <div className="container">
+              <Link className="navbar-brand" to="/">
+                  React Website
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link disabled" to="/contact">
-                  Contact
-                </Link>
-              </li>
-            </ul>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon" />
+              </button>
+              <div className="collapse navbar-collapse" id="navbarCollapse">
+                  <ul className="navbar-nav ml-auto">
+                      <li className="nav-item active">
+                          <Link className="nav-link" to="/">
+                              Home <span className="sr-only">(current)</span>
+                            </Link>
+                      </li>
+                      <li className="nav-item">
+                          <Link className="nav-link" to="/about">
+                              About
+                            </Link>
+                      </li>
+                      <li className="nav-item">
+                          <Link className="nav-link disabled" to="/contact">
+                              Contact
+                            </Link>
+                      </li>
+                  </ul>
+              </div>
           </div>
-        </div>
-      </nav>;
+      </nav>
+    );
   }
 }
 
